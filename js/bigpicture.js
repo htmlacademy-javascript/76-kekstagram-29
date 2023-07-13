43ee4edd815d6424cf8c6aa1b0e8b3663f12c446
Виктор <pershinvictor@mail.ru>
2023-07-13 13:11:07 +0300

    8 задание часть2

--- js/bigpicture.js
+++ js/bigpicture.js
@@ -1,10 +1,15 @@
 const bigPictureElement = document.querySelector('.big-picture');
-const commentCountElement = bigPictureElement.querySelector('.social__comment-count');
+const commentCountElement = bigPictureElement.querySelector('.comments-count'); //общее количество комментариев
+const commentShownCountElement = bigPictureElement.querySelector('.comments-count-show'); //подгруженные комментарии
 const commentListElement = bigPictureElement.querySelector('.social__comments');
-// const commentsLoaderElement = bigPictureElement.querySelector('.comments__loader');
+const commentsLoaderElement = bigPictureElement.querySelector('.comments-loader');
 const bodyElement = document.querySelector('body');
 const cancelButtonElement = bigPictureElement.querySelector('.big-picture__cancel');
 const commentElement = commentListElement.querySelector('.social__comment');
+
+const COMMENTS_LOAD = 5;
+let commentsShown = 0;
+let comments = [];
 
 //создаём комменты и подставляем в каждый из них данные из объекта
 const createComment = ({ avatar, name, message }) => {
@@ -18,17 +23,33 @@ const createComment = ({ avatar, name, message }) => {
   return comment;
 };
 
+const commentCounterCompare = () => {
+  commentShownCountElement.innerHTML = `${commentsShow} из <span class="comments-count">${comments.length}</span> комментариев`;
+}
+
 //генерируем комменты
-const renderComments = (comments) => {
-  commentListElement.innerHTML = '';
+const renderComments = () => {
+  commentsShown += COMMENTS_LOAD;
 
+  if (commentsShown >= comments.length) {
+    commentsLoaderElement.classList.add ('hidden');
+    commentsShown = comments.length;
+  } else {
+    commentsLoaderElement.classList.remove ('hidden');
+  }
   const fragment = document.createDocumentFragment();
-  comments.forEach((item) => {
-    const comment = createComment(item);
+  for (let i=0; i < commentsShown; i++) {
+    const comment = createComment (comments[i]);
     fragment.append(comment);
+  }
 
-  });
+  commentListElement.innerHTML = ' ';
   commentListElement.append(fragment);
+  commentShownCountElement.textContent = commentsShown;
+  commentCountElement.textContent = comments.length;
+
+  commentCounterCompare();
+
 };
 
 //функция прятать большое фото
@@ -36,6 +57,7 @@ const hideBigPicture = () => {
   bigPictureElement.classList.add('hidden');
   bodyElement.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
+  commentsShown = 0;
 
 };
 
@@ -45,6 +67,9 @@ function onDocumentKeydown(evt) {
     hideBigPicture();
   }
 };
+
+const onCommentsLoaderClick = () => renderComments ();
+
 
 const onCancelButtonClick = () => {
   hideBigPicture();
@@ -64,15 +89,18 @@ const renderPictureDetails = ({ url, likes, description }) => {
 const showBigPicture = (data) => {
   bigPictureElement.classList.remove('hidden');
   bodyElement.classList.add('modal-open');
-  // commentsLoaderElement.classList.add('hidden');
-  commentCountElement.classList.add('hidden');
+  commentsLoaderElement.classList.add('hidden');
   document.addEventListener('keydown', onDocumentKeydown);
 
   renderPictureDetails(data);
-  renderComments(data.comments); //отрисовываем под фоткой комментарии, берём данные из data
+  comments = data.comments;
+  if (comments.length > 0) {
+    renderComments();//отрисовываем под фоткой комментарии, берём данные из data
+  }
 
 };
 
 cancelButtonElement.addEventListener('click', onCancelButtonClick); //обработчик для закрытия окна фото по клику
+commentsLoaderElement.addEventListener('click', onCommentsLoaderClick);
 
 export { showBigPicture };
